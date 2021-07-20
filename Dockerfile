a8d3e79fef68b0b3258a52a6e0e5f25a5c815821
Danil Kudabaev <danil@unit.spb>
2021-07-20 16:00:29 +0300

    Fix bugs

--- Dockerfile
+++ Dockerfile
@@ -6,3 +6,5 @@ EXPOSE 8501:8501
 WORKDIR /app
 COPY app2.py /app/app.py
 COPY lrm/model /app/model/
+
+CMD ["streamlit", "run", "app.py"]
--- app2.py
+++ app2.py
@@ -5,3 +5,26 @@ import streamlit
 
 model = open("./model/linear_regression_model.pkl", "rb")
 lr = joblib.load(model)
+
+
+def lr_prediction(variables):
+    pred_arr = np.array(variables)
+    preds = pred_arr.reshape(1, -1)
+    preds = preds.astype(int)
+    mprediction = lr.predict(preds)
+    return mprediction
+
+
+def run():
+    streamlit.title("Linear Regression Model")
+    html_temp = """
+    """
+    streamlit.markdown(html_temp)
+    variables = [streamlit.text_input("Variable {}".format(i)) for i in range(1, 6)]
+    prediction = ""
+    if streamlit.button("Predict"):
+        prediction = lr_prediction(variables)
+    streamlit.success("The prediction by Model: {}".format(prediction))
+
+if __name__ == '__main__':
+    run()
